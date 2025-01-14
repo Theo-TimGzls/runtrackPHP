33a0c269edd0df677e19256fe15683543925b032
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-01-14 14:46:11 +0100

    ajout/job1 : creation d'une liste de nombre avec un nombre en gras et surligné

---
+++ jour2/job1/index.php
@@ -0,0 +1,21 @@
+<?php
+for ($i = 0; $i <= 1337; $i++) {
+    if ($i == 42) {
+        echo "<strong><u>" . $i . "</u></strong><br>";
+    } else {
+        echo $i . "<br>";
+    }
+}
+?>
+
+<!-- <?php
+$specialNumber = 42;
+
+for ($i = 0; $i <= 1337; $i++) {
+    if (isset($i) && $i == $specialNumber) {
+        echo "<b><u>$i</u></b><br>";
+    } else {
+        echo "$i<br>";
+    }
+}
+?> -->
