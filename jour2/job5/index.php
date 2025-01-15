d18566f9dfee878d6d6448155891db9f98e2463d
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-01-15 10:53:47 +0100

    ajout/jour2/job5 : creation d'une liste avec les nombres premiers seulement de 0 à 1000

---
+++ jour2/job5/index.php
@@ -0,0 +1,24 @@
+<?php
+function estPremier($n) {
+    if ($n <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$premiers = [];
+
+for ($i = 2; $i <= 1000; $i++) {
+    if (estPremier($i)) {
+        if (!isset($premiers[$i])) {
+            $premiers[$i] = true; 
+            echo $i . "<br>"; 
+        }
+    }
+}
+?>
