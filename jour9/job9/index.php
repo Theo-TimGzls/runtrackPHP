b778b43cf7a1943315cd3201e449ea881a944cc2
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-01-23 13:53:39 +0100

    ajout: récupération de notre base de donnée de PHPMyAdmin pour inséré dans PHP les infos des salles classé par ordre décroissant

---
+++ jour9/job9/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$username = 'root';
+$password = ''; 
+
+$db = new mysqli('localhost', $username, $password, 'jour8');
+
+$request = $db->prepare("SELECT * FROM salles WHERE capacite ORDER BY capacite DESC");
+$request->execute();
+$query = $request->get_result();
+
+echo "<table border='1'>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Id_etage</th>
+                    <th>Capacite</th>
+                </tr>
+            </thead>
+            <tbody>";
+while ($row = $query->fetch_array()) {
+    echo "<tr>
+            <td>" . $row['nom'] . "</td>
+            <td>" . $row['id_etage'] . "</td>
+            <td>" . $row['capacite'] . "</td>
+          </tr>";
+}
+echo "</tbody>
+      </table>";
+
+?>
