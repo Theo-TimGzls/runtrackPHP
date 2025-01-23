c24f71cd03db620f655c0aca30ec7131f03306cf
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-01-23 12:37:00 +0100

    ajout: récupération de notre base de donnée de PHPMyAdmin pour inséré dans PHP les infos des étudiants commencant par la lettre T

---
+++ jour9/job4/index.php
@@ -0,0 +1,35 @@
+<?php
+
+$username = 'root';
+$password = ''; 
+
+$db = new mysqli('localhost', $username, $password, 'jour8');
+
+$request = $db->prepare("SELECT * FROM etudiant WHERE prenom LIKE 'T%'");
+$request->execute();
+$query = $request->get_result();
+
+echo "<table border='1'>
+            <thead>
+                <tr>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th>Date de naissance</th>
+                    <th>Sexe</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>";
+while ($row = $query->fetch_array()) {
+    echo "<tr>
+            <td>" . $row['prenom'] . "</td>
+            <td>" . $row['nom'] . "</td>
+            <td>" . $row['naissance'] . "</td>
+            <td>" . $row['sexe'] . "</td>
+            <td>" . $row['email'] . "</td>
+          </tr>";
+}
+echo "</tbody>
+      </table>";
+
+?>
