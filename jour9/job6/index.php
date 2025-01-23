d0e5bdc85125b6a5902727998026c44d01ad4d0d
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-01-23 13:09:13 +0100

    ajout: récupération de notre base de donnée de PHPMyAdmin pour inséré dans PHP le nombre d'étudiant

---
+++ jour9/job6/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$username = 'root';
+$password = ''; 
+
+$db = new mysqli('localhost', $username, $password, 'jour8');
+
+$request = $db->prepare("SELECT COUNT(*) AS nb_etudiants FROM etudiant");
+$request->execute();
+$query = $request->get_result();
+$row = $query->fetch_assoc();
+
+echo "<table border='1'>
+            <thead>
+                <tr>
+                <th>Nombre d'étudiants</th>
+                </tr>
+            </thead>
+            <tbody>";
+echo "<tr>
+        <td>" . $row['nb_etudiants'] . "</td>
+      </tr>";
+echo "</tbody>
+      </table>";
+
+?>
